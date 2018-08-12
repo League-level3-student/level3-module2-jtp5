9c61aff40951c0c8a1a23223742c2015456baa04
jtp5 <league@WTS22.attlocal.net>
2018-08-12 14:26:45 -0700

    8/12/18

--- src/_00_Sorting_Algorithms/BubbleSorter.java
+++ src/_00_Sorting_Algorithms/BubbleSorter.java
@@ -1,16 +1,29 @@
 package _00_Sorting_Algorithms;
 
-public class BubbleSorter extends Sorter{
+public class BubbleSorter extends Sorter {
 	public BubbleSorter() {
 		type = "Bubble";
 	}
-	
-	//1. Use the bubble sorting algorithm to sort the array.
-	//   You can use display.updateDisplay() to show the current
-	//   progress on the graph.
+
+	// 1. Use the bubble sorting algorithm to sort the array.
+	// You can use display.updateDisplay() to show the current
+	// progress on the graph.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-		
+		boolean sorted = false;
+		int holder = 0;
+		while (!sorted) {
+			sorted = true;
+			for (int i = 0; i < array.length - 1; i++) {
+				if (array[i] > array[i + 1]) {
+					sorted = false;
+					holder = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = holder;
+					display.updateDisplay();
+				}
+			}
+		}
 	}
-	
+
 }
--- src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
+++ src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
@@ -1,33 +1,69 @@
 package _00_Sorting_Algorithms;
 
 public class _00_SortedArrayChecker {
-	//1. Write a static method called intArraySorted. 
-	//   This method takes in an array of integers
-	//   and it returns a boolean.
-	//   The method returns true if the integer
-	//   array is in ascending order and false otherwise
+	// 1. Write a static method called intArraySorted.
+	// This method takes in an array of integers
+	// and it returns a boolean.
+	// The method returns true if the integer
+	// array is in ascending order and false otherwise
+	public static boolean intArraySorted(int[] nums) {
+		boolean outcome = true;
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] > nums[i + 1]) {
+				outcome = false;
+			}
+		}
+		return outcome;
+	}
 
-	
-	//2. Write a static method called doubleArraySorted. 
-	//   This method takes in an array of doubles
-	//   and it returns a boolean.
-	//   The method returns true if the double
-	//   array is in ascending order and false otherwise
+	// 2. Write a static method called doubleArraySorted.
+	// This method takes in an array of doubles
+	// and it returns a boolean.
+	// The method returns true if the double
+	// array is in ascending order and false otherwise
 
-	
-	//3. Write a static method called charArraySorted. 
-	//   This method takes in an array of characters
-	//   and it returns a boolean.
-	//   The method returns true if the character
-	//   array is in alphabetical order and false otherwise
-	//   (You can compare characters just like integers)
+	public static boolean doubleArraySorted(double[] nums) {
+		boolean outcome = true;
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] > nums[i + 1]) {
+				outcome = false;
+			}
+		}
+		return outcome;
+	}
 
-	
-	//4. Write a static method called stringArraySorted. 
-	//   This method takes in an array of Strings
-	//   and it returns a boolean.
-	//   The method returns true if the String
-	//   array is in alphabetical order and false otherwise
-	//   (Use the compareTo(String) method)
+	// 3. Write a static method called charArraySorted.
+	// This method takes in an array of characters
+	// and it returns a boolean.
+	// The method returns true if the character
+	// array is in alphabetical order and false otherwise
+	// (You can compare characters just like integers)
+
+	public static boolean charArraySorted(char[] chars) {
+		boolean outcome = true;
+		for (int i = 0; i < chars.length - 1; i++) {
+			if (chars[i] > chars[i + 1]) {
+				outcome = false;
+			}
+		}
+		return outcome;
+	}
+
+	// 4. Write a static method called stringArraySorted.
+	// This method takes in an array of Strings
+	// and it returns a boolean.
+	// The method returns true if the String
+	// array is in alphabetical order and false otherwise
+	// (Use the compareTo(String) method)
+
+	public static boolean stringArraySorted(String[] words) {
+		boolean outcome = true;
+		for (int i = 0; i < words.length - 1; i++) {
+			if (words[i].compareTo(words[i + 1]) > 0) {
+				outcome = false;
+			}
+		}
+		return outcome;
+	}
 
 }
