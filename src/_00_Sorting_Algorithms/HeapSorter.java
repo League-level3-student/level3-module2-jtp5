1e84aea666bab45c1f4b7838dc17377f76eb8e57
Jones Pearlman <jones.pearlman@gmail.com>
2018-08-23 09:58:35 -0700

    worked on heapsort

--- src/_00_Sorting_Algorithms/HeapSorter.java
+++ src/_00_Sorting_Algorithms/HeapSorter.java
@@ -15,18 +15,24 @@ public class HeapSorter extends Sorter {
 	void sort(int[] array, SortingVisualizer display) {
 		//8. create an integer called n and set it equal 
 		//   to the length of the array
+		int n = array.length;
 		
 		//9. make a for loop that starts half way between
 		//   0 and n and counts down until it is less than 0.
 		//   Inside this for loop, call the heapSort method with n and i
-        
+        for (int i = n / 2; i > 0; i--) {
+			heapSort(array, n, i, display);
+		}
         //10. make a for loop that starts at n-1 
         //    and counts down until it is less than 0.
-        
+        for (int i = n - 1; i > 0; i--) {
+			
+		
         	//11. swap the array elements at 0 and i.
- 
+        	int holder = array[0];
+        	array[0] = array[i];
             //12. call the heapSort method with i and 0
-        
+        }
 	}
 	
 	public void heapSort(int[] array, int n, int i, SortingVisualizer display) {
@@ -46,11 +52,18 @@ public class HeapSorter extends Sorter {
         //   and array element at r is greater than array element at largest
         //   then set largest equal to r
         if(r < n && array[r] > array[largest]) {
-        	largest = l;
+        	largest = r;
         }
         //7. if largest is not equal to i
         //   then swap the array elements at i and largest.
         //   Also, call the heapSort method with n and largest
+        if(largest != i) {
+        	int holder = array[i];
+        	array[i] = array[largest];
+        	array[largest] = holder;
+        	display.updateDisplay();
+        	heapSort(array, n, largest, display);
+        }
 	}
 
 }
